0e83daddaac1fc9eb4ba69cb74b8aa44cd695ac1
Marcin Grzywaczewski <killavus@gmail.com>
2015-07-01 21:54:07 +0200

    Changed some HTML in index - added title and changed id of a div.
    Previously div ID didn't show its purpose - not it's clear what sits in
    it.

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,4 +5,4 @@ require('bootstrap/less/bootstrap.less');
 import React from 'react/addons';
 import ReactImmutableAltTodoApp from 'components/ReactImmutableAltTodoApp';
 
-React.render(<ReactImmutableAltTodoApp />, document.getElementById('content')); // jshint ignore:line
+React.render(<ReactImmutableAltTodoApp />, document.getElementById('todo-list')); // jshint ignore:line
