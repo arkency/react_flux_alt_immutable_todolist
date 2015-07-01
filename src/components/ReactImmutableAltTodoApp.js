c292979bac9acf8ccdf63b1201fc72a04aae0212
Marcin Grzywaczewski <killavus@gmail.com>
2015-07-01 20:48:03 +0200

    Created a project. It was generated using Yeoman and generator-react-webpack (https://github.com/newtriks/generator-react-webpack). I chose not to integrate Alt by default (it can be outdated so I will install it by myself) and chosen LESS as a stylesheet engine (since Bootstrap has LESS stylesheets integrated).

---
+++ src/components/ReactImmutableAltTodoApp.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var React = require('react/addons');
+var ReactTransitionGroup = React.addons.TransitionGroup;
+
+// CSS
+require('normalize.css');
+require('../styles/main.css');
+
+var imageURL = require('../images/yeoman.png');
+
+var ReactImmutableAltTodoApp = React.createClass({
+  render: function() {
+    return (
+      <div className='main'>
+        <ReactTransitionGroup transitionName="fade">
+          <img src={imageURL} />
+        </ReactTransitionGroup>
+      </div>
+    );
+  }
+});
+React.render(<ReactImmutableAltTodoApp />, document.getElementById('content')); // jshint ignore:line
+
+module.exports = ReactImmutableAltTodoApp;
