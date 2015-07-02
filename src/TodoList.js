39f8647f9887cfbd2deded3af64b3e383f7d9784
Marcin Grzywaczewski <killavus@gmail.com>
2015-07-02 14:22:13 +0200

    Removed the existing component for displaying a Yeoman logo.
    I don't need it. I'll make my components from scratch. Since I want to
    remove dead code as soon as possible, I decided to do it now.

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,3 @@
 
 require('bootstrap/less/bootstrap.less');
 
-import React from 'react/addons';
-import ReactImmutableAltTodoApp from 'components/ReactImmutableAltTodoApp';
-
-React.render(<ReactImmutableAltTodoApp />, document.getElementById('todo-list')); // jshint ignore:line
