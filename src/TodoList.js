d7f7fe914730f06f255cf0b9c46341492d382e6e
Marcin Grzywaczewski <killavus@gmail.com>
2015-07-03 02:11:46 +0200

    Modified main application file to render TodoList component.
    For now it is impossible to add new tasks, but it renders an empty state
    correctly and has a proper state. You can check it by yourself by
    console.logging state in a render method.
    
    To check whether it updates if you call an add action, I'd recommend to
    import TodoListActions in this file and call the action in code. I
    tested it that way.

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,3 +2,8 @@
 
 require('bootstrap/less/bootstrap.less');
 
+import React    from 'react/addons';
+import TodoList from 'components/TodoList';
+
+React.render(<TodoList />, document.getElementById('todo-list'));
+
